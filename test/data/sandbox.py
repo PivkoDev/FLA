81b67bad1fb213706e415d7fd90a9c221fb238a7
PivkoDev <bartosz.piwkowski@gmail.com>
2020-01-29 14:48:40 +0100

    pandas added

--- test/data/sandbox.py
+++ test/data/sandbox.py
@@ -3,16 +3,27 @@
 @brief: one file for all code experiments and checks
 '''
 
+import pandas as pd
+
 MANUFACTURERS = ['BMW', 'Lotus', "Mazda", "Suzuki", "Volvo"]
+CARS_SOLD = [2000, 100, 5000, 10000, 8000]
+INVENTORY = []
 
 class Car(object):
     def __init__(self):
         pass
 
 if __name__ == "__main__":
-    print("sandbox run ...\n")
-
-    print("manufacturers:\n")
+    print("sandbox run ...")
+    print("manufacturers:")
 
     for i, item in enumerate(MANUFACTURERS):
-        print ("{0} : {1}".format(i, item))
+      INVENTORY.append("{0} : {1}".format(i, item))
+      print(INVENTORY[-1])
+
+    cars_sold = pd.DataFrame(MANUFACTURERS, CARS_SOLD)
+
+    print(cars_sold.head())
+
+
+    print("end.")
